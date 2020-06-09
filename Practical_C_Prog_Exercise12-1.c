24e8b23d2c935a9fe66a9c65cfa04bb08b26c3b6
alex-rt <61000223+alex-rt@users.noreply.github.com>
2020-06-08 20:46:40 -0500

    Update Practical_C_Prog_Exercise12-1.c

--- Practical_C_Prog_Exercise12-1.c
+++ Practical_C_Prog_Exercise12-1.c
@@ -39,7 +39,7 @@ int main()
 	listlen=sizeof(list)/sizeof(list[0]);//finding how many subscribers are there
 
 	for(i=0;i<listlen;i++){//printing all suscribers of the list
-		printf("***Subsriber No. %d***\n", (i+1));
+		printf("***Subsriber No.%d***\n", (i+1));
 		printf("First name:%s\n", list[i].firstname);
 		printf("Last name:%s\n", list[i].lastname);
 		printf("Gender:%s\n", list[i].gender);
