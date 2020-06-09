8838227aa47c159c867b98980b8daf245ef56cc7
alex-rt <61000223+alex-rt@users.noreply.github.com>
2020-06-08 20:45:45 -0500

    Update Practical_C_Prog_Exercise12-1.c

--- Practical_C_Prog_Exercise12-1.c
+++ Practical_C_Prog_Exercise12-1.c
@@ -5,17 +5,17 @@
 
 int main()
 {
-	int list_length;//mailing list length 
-	int counter;//counter used to loop through all list entries 
+	int listlen;//mailing list length 
+	int i;//counter used to loop through all list entries 
 
-	struct mailing {
-		char first_name[20];
-		char last_name[30];
+	struct mailing {//mailing struct to hold all the data
+		char firstname[20];
+		char lastname[30];
 		char gender[6];
 		char email[40];
 	};
 
-	struct mailing list[] = {
+	struct mailing list[]={//doing the list of subscribers, 3 in this case
 		{
 			"Alejandro",
 			"Rodriguez",
@@ -28,16 +28,22 @@ int main()
 			"Male",
 			"yeezus@gmail.com",
 		},
+		{
+			"Frank",
+			"Ocean",
+			"Male",
+			"blonded@gmail.com",
+		},
 	};
 
-	list_length = sizeof(list) / sizeof(list[0]);
+	listlen=sizeof(list)/sizeof(list[0]);//finding how many subscribers are there
 
-	for (counter = 0; counter < list_length; ++counter) { //printing all suscribers of the list
-		printf("**Subsriber No. %d**\n", (counter + 1));
-		printf("First name: %s\n", list[counter].first_name);
-		printf("Last name: %s\n", list[counter].last_name);
-		printf("Gender: %s\n", list[counter].gender);
-		printf("Email: %s\n", list[counter].email);
+	for(i=0;i<listlen;i++){//printing all suscribers of the list
+		printf("***Subsriber No. %d***\n", (i+1));
+		printf("First name:%s\n", list[i].firstname);
+		printf("Last name:%s\n", list[i].lastname);
+		printf("Gender:%s\n", list[i].gender);
+		printf("Email:%s\n", list[i].email);
 
 		printf("\n");
 	}
