95a94095b87542dd571029ae9954bddf4be4bf81
alex-rt <61000223+alex-rt@users.noreply.github.com>
2020-07-19 18:08:04 -0500

    Create exercise2.py

---
+++ numpymix/exercise2.py
@@ -0,0 +1,49 @@
+#EXERCISE 2 .PY
+#The following program will:
+"""
+1. Create a 4x4 array full of zeros and print it
+2. Add row values from 0 to 4 to fill array, print changes
+3.finding the transpose od the x matrix, print x_transpose
+4.Effectuate a dot product of inicial matrix and its transpose and print result
+5.Multiply result by a scalar(3 in this case)
+6.Display final result to user
+"""
+#Sample Input:
+  """
+None
+"""
+
+#Expected Output:
+  """
+Original array:
+[[0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0.]]
+Row values ranging from 0 to 4.
+[[0. 1. 2. 3. 4.]
+ [0. 1. 2. 3. 4.]
+ [0. 1. 2. 3. 4.]
+ [0. 1. 2. 3. 4.]
+ [0. 1. 2. 3. 4.]]
+Transpose matrix:
+[[0. 0. 0. 0. 0.]
+ [1. 1. 1. 1. 1.]
+ [2. 2. 2. 2. 2.]
+ [3. 3. 3. 3. 3.]
+ [4. 4. 4. 4. 4.]]
+Dot product x*xtranspose:
+[[30. 30. 30. 30. 30.]
+ [30. 30. 30. 30. 30.]
+ [30. 30. 30. 30. 30.]
+ [30. 30. 30. 30. 30.]
+ [30. 30. 30. 30. 30.]]
+Previous result * 3 :
+[[90. 90. 90. 90. 90.]
+ [90. 90. 90. 90. 90.]
+ [90. 90. 90. 90. 90.]
+ [90. 90. 90. 90. 90.]
+ [90. 90. 90. 90. 90.]]
+ """
+ 
