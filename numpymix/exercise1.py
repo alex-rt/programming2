c7a91842d66f0e19504a94234b9cd3872970c288
alex-rt <61000223+alex-rt@users.noreply.github.com>
2020-07-19 17:31:12 -0500

    Update exercise1.py

--- numpymix/exercise1.py
+++ numpymix/exercise1.py
@@ -1,1 +1,33 @@
 #EXERCISE 1 .PY
+#The following program will:
+"""
+1. Get a list of values and promt to user
+2. Turn them into a one-dimentional array and print it
+3.Change a specific value based on its position, print changes
+4.Get the data type, print it
+5. Update data type to float, double values and print all visible changes
+6.Create a new array from x values to do a unit conversion
+7.Take new array as Farhenheit values and display them to user
+8.Convert to Centigrade unit and show user final result.
+"""
+#Sample Input:
+  """
+l = [12, 13, 100, 36, 43, 45, 37 ]
+"""
+
+#Expected Output:
+  """
+Original List: [12, 13, 100, 36, 43, 45, 37]
+One-dimensional NumPy array:  [ 12  13 100  36  43  45  37]
+Update sixth value to 38
+[ 12  13 100  36  43  45  38]
+Type of array: int64
+Array converted to a float type and doubled values:
+[ 24.  26. 200.  72.  86.  90.  76.]
+Updated array type:  float64
+Values in Fahrenheit degrees:
+[ 24.  26. 200.  72.  86.  90.  76.]
+Values in  Centigrade degrees:
+[-4.44444444 -3.33333333 93.33333333 22.22222222 30.         32.22222222
+ 24.44444444]
+ """
